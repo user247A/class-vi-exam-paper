50c3ca5ff40068ccb66ca3e1c87458e95ac42d02
user247A <exp3160@gmail.com>
2025-04-07 12:49:59 +0530

    Update document.js

--- backend/models/document.js
+++ backend/models/document.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 
 const documentSchema = new mongoose.Schema({
-  header: String,
-  fileName: String,
-  fileType: String,
-  filePath: String,
-  fileURL: String
+  header: {
+    type: String,
+    required: true,
+  },
+  fileName: {
+    type: String,
+    required: true,
+  },
+  fileType: {
+    type: String,
+    required: true,
+  },
+  filePath: {
+    type: String,
+    required: true,
+  },
+  fileURL: {
+    type: String,
+    required: true,
+  }
+}, {
+  timestamps: true // adds createdAt and updatedAt automatically
 });
 
 module.exports = mongoose.model("Document", documentSchema);
